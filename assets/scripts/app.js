d472eea134f110f916c2342119f23b846d9f00f1
water-sauce <calvin@watersauce.com>
2018-11-24 11:42:59 -0800

    clean up, adding more comments

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,37 +4,37 @@
 // expand the image
 function expandImg( img ){
 	// attach image to the overlay
-	let expanded_img = document.getElementById( 'expanded-image' ),
-		expanded_img_wrapper = document.getElementById( 'expanded-image-wrapper' );
+	let expanded_img = document.getElementById( "expanded-image" ),
+		expanded_img_wrapper = document.getElementById( "expanded-image-wrapper" );
 		// since button does not have the src, change to a data attribute to be able to get the image
-		expanded_img.src = img.srcElement.getAttribute( 'data-pull' );
-		expanded_img.setAttribute( 'alt', img.srcElement.alt + ' (expanded image)' );
-		expanded_img_wrapper.classList.add( 'expand' );
+		expanded_img.src = img.srcElement.getAttribute( "data-pull" );
+		expanded_img.setAttribute( "alt", img.srcElement.alt + " (expanded image)" );
+		expanded_img_wrapper.classList.add( "expand" );
 		// prevent scroll when open
-		document.getElementsByTagName("body")[0].setAttribute( 'style', 'overflow:hidden' );
+		document.getElementsByTagName( "body" )[ 0 ].setAttribute( "style", "overflow:hidden" );
 }
 
 // close the image
 function closeExpandedImg(){
-	let expanded_img_wrapper = document.getElementById( 'expanded-image-wrapper' ),
+	let expanded_img_wrapper = document.getElementById( "expanded-image-wrapper" ),
 		// remove the dog image from the div
-		expanded_img = document.getElementById( 'expanded-image' );
-		expanded_img.src = '';
-		expanded_img.setAttribute( 'alt', '' );
-		expanded_img_wrapper.classList.remove( 'expand' );
+		expanded_img = document.getElementById( "expanded-image" );
+		expanded_img.src = "";
+		expanded_img.setAttribute( "alt", "" );
+		expanded_img_wrapper.classList.remove( "expand" );
 		// enable page scroll after close
-		document.getElementsByTagName("body")[0].setAttribute( 'style', '' );
+		document.getElementsByTagName( "body" )[ 0 ].setAttribute( "style", "" );
 }
 
 function createFilters( dog_arr, arr_name ){
-	let gallery_filter = document.getElementById( 'image-gallery-filters' ),
-		filter_list = document.getElementById( 'filter-list' ),
-		filter_category = document.createElement( 'li' ),
-		filter_name = document.createElement( 'h4' ),
-		filter_expand = document.createElement( 'a' ),
-		filter_ul = document.createElement( 'ul' );
+	let gallery_filter = document.getElementById( "image-gallery-filters" ),
+		filter_list = document.getElementById( "filter-list" ),
+		filter_category = document.createElement( "li" ),
+		filter_name = document.createElement( "h4" ),
+		filter_expand = document.createElement( "a" ),
+		filter_ul = document.createElement( "ul" );
 
-	// flatten array and reduce to get the name and number of groupings
+	// flatten array and use reduce function to get the name and number of groupings
 	dog_arr = dog_arr.flat().reduce(function(dog, type){
 		if ( !dog[type] ){
 			dog[type] = 0;
@@ -43,28 +43,29 @@ function createFilters( dog_arr, arr_name ){
 		return dog;
 	}, {});
 
-	filter_expand.innerHTML = '+';
-	filter_expand.classList.add( 'expander' );
-	filter_expand.addEventListener( 'click', function( e ){
-		this.classList.toggle( 'expanded' );
-		this.parentNode.classList.toggle( 'expand' );
+	// add the expand button for mobile
+	filter_expand.innerHTML = "+";
+	filter_expand.classList.add( "expander" );
+	filter_expand.addEventListener( "click", function( e ){
+		this.classList.toggle( "expanded" );
+		this.parentNode.classList.toggle( "expand" );
 	});
 
 	filter_name.innerHTML = arr_name;
-	filter_category.classList.add( 'category-list' );
+	filter_category.classList.add( "category-list" );
 	filter_category.appendChild( filter_name );
 	filter_category.appendChild( filter_expand );
 	filter_category.appendChild( filter_ul );
 
 	Object.keys( dog_arr ).forEach( function( key ) {
-		let checkbox = document.createElement( 'input' ),
-			name = document.createElement( 'span' ),
-			checkbox_ul = document.createElement( 'ul' ),
-			checkbox_li = document.createElement( 'li' );
+		let checkbox = document.createElement( "input" ),
+			name = document.createElement( "span" ),
+			checkbox_ul = document.createElement( "ul" ),
+			checkbox_li = document.createElement( "li" );
 
-		checkbox.setAttribute( 'type', 'checkbox' );
-		checkbox.setAttribute( 'data-set', key );
-		name.innerHTML = key + ' (' + dog_arr[ key ] + ')';
+		checkbox.setAttribute( "type", "checkbox" );
+		checkbox.setAttribute( "data-set", key );
+		name.innerHTML = key + " (" + dog_arr[ key ] + ")";
 
 		checkbox_li.appendChild( checkbox );
 		checkbox_li.appendChild( name );
@@ -78,12 +79,14 @@ function createFilters( dog_arr, arr_name ){
 // this function creates the image gallery for the dogs and lazy loads the images
 function createGallery( dogs ){
 
-	let gallery_div = document.getElementById( 'image-gallery' ),
-		total_dogs = document.getElementById( 'total-dogs-showing' ),
+	let gallery_div = document.getElementById( "image-gallery" ),
+		total_dogs = document.getElementById( "total-dogs-showing" ),
+		// create empty arrays to be passed to the filter creationx
 		breed_arr = [],
 		gender_arr = [],
 		age_arr = [];
-		
+	
+	// show total length
 	total_dogs.innerHTML = dogs.length;
 
 	dogs.map( function( dog, index ){
@@ -102,72 +105,73 @@ function createGallery( dogs ){
 			breed_wrapper = document.createElement( "span" ),
 			info_h5 = document.createElement( "h5" ),
 			age = document.createTextNode( dog.age ),
-			gender = document.createTextNode( ' ' + dog.sex ),
+			gender = document.createTextNode( " " + dog.sex ),
 			read_more = document.createElement( "a" ),
 			info_wrapper = document.createElement( "figcaption" ),
 			description_wrapper = document.createElement( "p" ),
 			description = document.createTextNode( dog.description );
 			
 		// add the image
-		image.setAttribute( 'data-src', dog.image );
-		image.setAttribute( 'alt', dog.name + ' | located in San Francisco | ' + dog.description );
-		image.classList.add( 'lazyload' );
-		image_wrapper.classList.add( 'image-wrapper' );
-		image_expander.setAttribute( 'src', './assets/images/custom/expand-btn.png' );
-		image_expander.setAttribute( 'data-pull', dog.image );
-		image_expander.setAttribute( 'alt', 'Expand image');
-		image_expander.classList.add( 'image-expander');
+		image.setAttribute( "data-src", dog.image );
+		image.setAttribute( "alt", dog.name + " | located in San Francisco | " + dog.description );
+		image.classList.add( "lazyload" );
+		image_wrapper.classList.add( "image-wrapper" );
+		image_expander.setAttribute( "src", "./assets/images/custom/expand-btn.png" );
+		image_expander.setAttribute( "data-pull", dog.image );
+		image_expander.setAttribute( "alt", "Expand image");
+		image_expander.classList.add( "image-expander");
 		// add the image expand button and have the event listener on it
-		image_expander.addEventListener( 'click', expandImg.bind( this ) );
+		image_expander.addEventListener( "click", expandImg.bind( this ) );
 		image_wrapper.appendChild( image );
 		image_wrapper.appendChild( image_expander );
 		card.appendChild( image_wrapper );
 
 		// add the name of the dog
 		name_holder.appendChild( name );
-		name_holder.classList.add( 'dog-name' );
+		name_holder.classList.add( "dog-name" );
 		info_wrapper.appendChild( name_holder );
 
 		// add the age and gender of the dog
 		info_h5.appendChild( age );
 		info_h5.appendChild( gender );
-		info_h5.classList.add( 'capitalize' );
+		info_h5.classList.add( "capitalize" );
 		info_wrapper.appendChild( info_h5 );
 
 		// add the dog breed
-		breed = breed.split(",").join(", ");
+		breed = breed.split( "," ).join( ", " );
 		breed_wrapper.innerHTML = breed;
 		info_h5.appendChild( breed_wrapper );
 
 		// add the description of the dog
 		description_wrapper.appendChild( description );
-		description_wrapper.classList.add( 'description' );
+		description_wrapper.classList.add( "description" );
 		info_wrapper.appendChild( description_wrapper );
 
 		// add the link to the dog page
 		read_more.innerHTML = "Learn More";
 		read_more.classList.add( "button", "button-read-more" );
+		read_more.setAttribute( "href", "#" );
 		info_wrapper.appendChild( read_more );
 
 		// add all the image cards to the gallery div
 		card.appendChild( info_wrapper );
-		card.classList.add( 'card' );
+		card.classList.add( "card" );
 		gallery_div.appendChild( card );
 	})
 
-	createFilters( breed_arr, 'Breed' );
-	createFilters( gender_arr, 'Gender' );
-	createFilters( age_arr, 'Age' );
+	createFilters( breed_arr, "Breed" );
+	createFilters( gender_arr, "Gender" );
+	createFilters( age_arr, "Age" );
 }
 
 function openMobileMenu(){
-	let mobile_menu = document.getElementById( 'mobile-menu' );
-	mobile_menu.classList.add( 'open' );
+	let mobile_menu = document.getElementById( "mobile-menu" );
+	mobile_menu.classList.add( "open" );
 }
 
 function closeMobileMenu(){
-	let mobile_menu = document.getElementById( 'mobile-menu' );
-	mobile_menu.classList.remove( 'open' );
+	let mobile_menu = document.getElementById( "mobile-menu" );
+	mobile_menu.classList.remove( "open" );
 }
 
 
